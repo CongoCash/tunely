40a711670d949a330fa64d5c46c30ef9480d7457
David <davjue@gmail.com>
2017-08-31 11:49:41 -0700

    finished sprint 0 and part of sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -37,16 +37,60 @@ sampleAlbums.push({
 
 
 
-$(document).ready(function() {
-  console.log('app.js loaded!');
-});
-
-
-
 
 
 // this function takes a single album and renders it to the page
+$(document).ready(function() {
+    $.ajax({
+        method: "GET",
+        url: "/api/albums",
+        json: true
+    })
+    .then(function(data) {
+        data.forEach(function(album) {
+            $('#albums').append(renderAlbum(album));
+        })
+    })
+})
 function renderAlbum(album) {
   console.log('rendering album:', album);
+  return `
+<div class="row album">
 
+            <div class="col-md-10 col-md-offset-1">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                <div class='row'>
+                    <div class="col-md-3 col-xs-12 thumbnail album-art">
+                      <img src="/images/800x800.png" alt="album image">
+                    </div>
+
+                    <div class="col-md-9 col-xs-12">
+                      <ul class="list-group">
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Album Name:</h4>
+                          <span class='album-name'>${album.name}</span>
+                        </li>
+
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Artist Name:</h4>
+                          <span class='artist-name'>${album.artistName}</span>
+                        </li>
+
+                        <li class="list-group-item">
+                          <h4 class='inline-header'>Released date:</h4>
+                          <span class='album-releaseDate'>${album.releaseDate}</span>
+                        </li>
+                      </ul>
+                    </div>
+
+                  </div>
+
+                  <div class='panel-footer'>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>`
 }
---
+++ server.js
@@ -0,0 +1,18 @@
+let express = require("express");
+let app = express();
+
+app.get("/", function(req, res) {
+    res.sendFile("views/index.html", {root: __dirname})
+    console.log("it worked");
+});
+
+app.use(express.static('public'));
+
+var db = require('./models');
+
+var controllers = require('./controllers');
+
+app.get('/api', controllers.api.index);
+
+app.listen(3000, function() {
+});
